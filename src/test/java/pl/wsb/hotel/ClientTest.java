8a00d82660e1bc4e8427585bc8d0886f8ebfd5b3
phajw <dscr@duck.com>
2024-03-17 17:20:33 +0100

    Remove redundant tests

---
+++ src/test/java/pl/wsb/hotel/ClientTest.java
@@ -0,0 +1,58 @@
+package pl.wsb.hotel;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getBirthDate() {
+    }
+
+    @Test
+    void getFullName() {
+    }
+
+    @Test
+    void getAge() {
+    }
+
+    @Test
+    void setBirthDate() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+}
